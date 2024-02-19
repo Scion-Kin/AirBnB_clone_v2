b813e3ba40a1ba168642d1f3375487ab921a0d89
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-19 21:14:33 +0100

    remove comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,7 @@
 -- create server for AirBnb project
 
-
--- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- grant privileges
 GRANT ALL PRIVILEGES on hbnb_dev_db.* to 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 GRANT SELECT on performance_schema to 'hbnb_dev'@'localhost';
