f75e896b853f9f1c605eb36f360e0d8449d7ff0c
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-19 21:05:27 +0100

    rephrase commands

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,10 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create user
-CREATE USER hbnb_dev@localhost IF NOT EXISTS IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER 'hbnb_dev'@'localhost' IF NOT EXISTS IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant privileges
-GRANT ALL on hbnb_dev_db to hbnb_dev@localhost;
-GRANT SELECT on performance_schema to hbnb_dev@localhost;
+GRANT ALL PRIVILEGES on hbnb_dev_db to 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT on performance_schema to 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 
