2310173d9088450604bdf8339157fe321dd377c7
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-19 21:17:40 +0100

    remove comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES on hbnb_dev_db.* to 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-GRANT SELECT on performance_schema to 'hbnb_dev'@'localhost';
+GRANT SELECT on performance_schema.* to 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
