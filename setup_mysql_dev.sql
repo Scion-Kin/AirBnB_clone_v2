a438724609c1037e9394a377d1a3f79ceaa38ac5
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-19 21:13:04 +0100

    new commands to User creation and granting privleges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,9 +4,9 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create user
-CREATE USER 'hbnb_dev'@'localhost' IF NOT EXISTS IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant privileges
-GRANT ALL PRIVILEGES on hbnb_dev_db to 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES on hbnb_dev_db.* to 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 GRANT SELECT on performance_schema to 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
