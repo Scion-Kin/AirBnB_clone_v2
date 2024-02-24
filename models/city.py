bff79d4541b735753e8f4fbd6f32996907d3f88d
Scion-Kin <dinturner17@gmail.com>
2024-02-24 21:03:58 +0200

    Solved import errors and delete method in console.py now works well

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
