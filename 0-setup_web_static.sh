fa7d1b65edcebfd22573d626abcdc1eb741da7f9
Scion-Kin <dinturner17@gmail.com>
2024-03-10 11:38:32 +0200

    Task completed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
-# Install nginx and create folders
-# check nginx existence and install if it does not exist
+# This script sets up a new nginx server to deploy a static file
 
-nginx_check=$(whereis nginx | cut -d ":" -f 2| wc -w) > /dev/null
-if [ "${nginx_check}" -le 1 ];
-then
-	sudo apt update
-	sudo apt install -y nginx
-	sudo service nginx start
-else
-	[ -d /data/web_static/releases/test ] || mkdir -p /data/web_static/releases/test
-	[ -d /data/web_static/shared ] || mkdir -p /data/web_static/shared
-	if [ -L /data/web_static/current ]; 
-	then 
-		rm /data/web_static/current
-		ln -s /data/web_static/releases/test /data/web_static/current
-	else
-		ln -s /data/web_static/releases/test /data/web_static/current
-	fi
-	echo "Holberton School" > /data/web_static/releases/test/index.html
-	chown -R ubuntu:ubuntu /data/
+apt-get update -y
+apt-get install nginx -y
+
+mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+#automatically created directories '/data/', '/data/web_static/' and '/data/web_static/releases'
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+if [ -L /data/web_static/current ]; then
+    rm /data/web_static/current
+    # symbolic link removed if it exits
 fi
 
-# update Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-# create a backup of config file
-cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# add the location /hbnb_static/ { } block to the default Nginx configuration
-sed -ie 's/^\tlocation \//\tlocation \/hbnb_static {\n\talias /data/web_static/current/' /etc/nginx/sites-available/default
-	
+chown -R ubuntu:ubuntu /data/
+
+conf=\
+"
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+	}
+"
+
+sed -i "47i$conf" /etc/nginx/sites-enabled/default
+
+service nginx restart
