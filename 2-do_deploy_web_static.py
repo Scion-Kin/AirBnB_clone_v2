cdb121ef5a030c5ca9ef81775eb6c22a592a91b7
Scion-Kin <dinturner17@gmail.com>
2024-03-10 22:50:49 +0200

    Task completed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,21 @@ env.hosts = ['54.175.223.158', '52.91.131.105']
 env.usr = 'ubuntu'
 
 
+def do_pack():
+    ''' This function creates an archive and stores it in versions folder '''
+
+    if not os.path.exists('versions'):
+        local('mkdir versions')
+
+    now = datetime.now()
+    arch_file = 'versions/web_static{}.tgz'.format(
+        now.strftime("%Y%m%d%H%M%S")
+    )
+
+    command = local("tar -cvzf {} web_static".format(arch_file))
+    if not command.failed:
+        return arch_file
+
 def do_deploy(archive_path):
     ''' Deploys a version to the web servers '''
 
