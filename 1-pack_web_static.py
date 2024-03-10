485add6eec616025fca2d76529e58d352334eab8
Scion-Kin <dinturner17@gmail.com>
2024-03-10 16:58:58 +0200

    Task completed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,21 +2,19 @@
 ''' This is a fabric file that sets up a tar archive '''
 
 from datetime import datetime
-from fabric.api import *
-
+from fabric.api import local
+import os
 
 def do_pack():
     ''' This function creates an archive and stores it in versions folder '''
 
+    if os.path.exists('versions'):
+        local('mkdir versions')
+
     now = datetime.now()
-    arch_file = 'versions/web_static_{}{}{}{}{}{}.tgz'\
-        .format(now.year, now.month, now.day,
-                now.hour, now.minute, now.second)
+    arch_file = 'versions/web_static_{}.tgz'\
+        .format(now.strftime("%Y%m%d%H%M%S"))
 
-    try:
-        command = local('tar -cvzf {} web_static'.format(arch_file))
-
-    except Exception:
-        return None
-
-    return 'versions/{}'.format(arch_file)
+    command = local("tar -cvzf {} {}".format(name, "web_static"))
+    if not command.failed:
+        return arch_file
