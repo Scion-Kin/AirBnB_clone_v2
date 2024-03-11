33730ec0214cb69ab8ac3138ed60642c563e1ba3
Scion-Kin <dinturner17@gmail.com>
2024-03-11 15:50:29 +0200

    Final update to deployment bash script

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,26 @@ if [ $# != 3 ]; then
 fi
 
 servers=("$1" "$2")
-file=$3
+inputFile=$3
+dtime="$(date +'%Y%m%d%H%M%S')"
+file="$inputFile$dtime"
 
 echo ""
 echo "After this operation, the deployed version will be stored in the folder 'versions/'"
-echo "Deployment will commence in five seconds. Check if you entered correct information."
+echo "Deployment will commence in 10 seconds. Check if you entered correct information."
 echo "Press ctrl c, to cancel if you made a mistake."
 echo ""
 
-for ((j=5;j>0;j--)); do
+for ((j=9;j>-1;j--)); do
     sleep 1
-    echo "$j"
+    echo -ne "Starting deployment in $j\r"
 done
 clear
 
-echo "The directory $file will be deployed to the following servers: "
+echo "Your new archive will be named:        $file"
+echo ""
+
+echo "Deploying to the following servers: "
 echo ""
 for i in "${servers[@]}"; do
     printf "        %s\n" "$i" 
@@ -33,7 +38,7 @@ if [ ! -d versions ]; then
     mkdir "versions/"
 fi
 
-tar -czf "versions/$file.tgz" "$file"
+tar -czf "versions/$file.tgz" "$inputFile"
 
 for i in "${servers[@]}"; do
     echo ""
@@ -41,23 +46,24 @@ for i in "${servers[@]}"; do
     echo ""
     echo "Copied archive file to server:        $i in directory:        /tmp/"
     echo ""
-    echo "Checking and removing an existing web_static directory on the desired location ..."
-    echo ""
-    ssh ubuntu@"$i" "sudo rm -rf /data/web_static/releases/web_static && ls /tmp/ | grep web_static"
-    echo ""
     echo "Extracting archive to:        /data/web_static/releases/ ..."
-    ssh ubuntu@"$i" "tar -xzf /tmp/$file.tgz -C /data/web_static/releases/"
     echo ""
-    ssh ubuntu@"$i" "sudo rm -rf /data/web_static/current && ln -s /data/web_static/releases/web_static /data/web_static/current"
+    ssh ubuntu@"$i" "mkdir /data/web_static/releases/new && tar -xzf /tmp/$file.tgz -C /data/web_static/releases/new" 
+    ssh ubuntu@"$i" "mv /data/web_static/releases/new/web_static /data/web_static/releases/$file"
+    echo "Here are the new contents of the releases directory: "
+    echo ""
+    ssh ubuntu@"$i" "sudo rm -r /data/web_static/releases/new/ && ls /data/web_static/releases/ | sed 's/^/\t\t\t/'"
+    echo ""
+    ssh ubuntu@"$i" "sudo rm -rf /data/web_static/current && ln -s /data/web_static/releases/$file /data/web_static/current"
     echo "Finished making a symbolic link for the new web static on server:        $i"
     echo ""
-    ssh ubuntu@"$i" "sudo rm -rf /tmp/$file.tgz && ls -l /data/web_static/ | grep current && sudo service nginx restart"
+    ssh ubuntu@"$i" "sudo rm -rf /tmp/$file.tgz && sudo service nginx restart"
     echo ""
     echo "Deleted the archive from /tmp/, and restarted the Nginx server."
     echo ""
 done
 
-echo "Your /hbnb_static/ is now live! You can visit: "
+echo "Your newest release ( $file ) of web_static is now live on /hbnb_static/! You can visit: "
 echo ""       
 echo "                                     ${servers[0]}/hbnb_static/100-index.html or"
 echo ""
