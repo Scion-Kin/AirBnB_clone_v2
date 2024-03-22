37f2cf2c6d9c97a0a5b9d8b06df51829d4b64418
Scion-Kin <dinturner17@gmail.com>
2024-03-22 13:49:02 +0200

    Task completed

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' A Flask hello world app '''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+Welcome to Flask. A very interesting web framework
