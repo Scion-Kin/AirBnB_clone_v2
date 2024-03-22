2b507c3180b9378059c39f7a5756640043385bdd
Scion-Kin <dinturner17@gmail.com>
2024-03-22 13:51:45 +0200

    Task completed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,8 +3,8 @@
 
 from flask import Flask
 
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
@@ -13,4 +13,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
