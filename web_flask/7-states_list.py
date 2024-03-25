94be77e4238decf31e24d589573e122007b12d6e
Scion-Kin <dinturner17@gmail.com>
2024-03-25 19:46:38 +0200

    Task completed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,19 +3,22 @@
 starts a Flask web application
 """
 
-#import sys, os
-
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), '..')))
-
+import sys
+import os
 from flask import Flask, render_template
-from models import *
-from models import storage
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__name__), '..')))
+# This will add the parent directory name to sys.path
+# This will help when this flask app is started from it's directory
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with the states listed in alphabetical order"""
+    from models import storage
+
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
@@ -23,7 +26,10 @@ def states_list():
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
+    from models import storage
+
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
